373f814c6df4c848129a948b691af02d201c9387
Josef Friedrich <josef@friedrich.rocks>
2016-08-24 23:25:26 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='phrydy',
-      version='0.2',
+      version='0.3',
       description='A easy wrapper for mutagen',
       url='https://github.com/Josef-Friedrich/phrydy',
       author='Josef Friedrich',
