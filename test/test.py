a3c3600128a52cf380c07c205b4acc062b1aee5c
Josef Friedrich <josef@friedrich.rocks>
2016-08-30 15:23:57 +0200

    Add some simple tests

---
+++ test/test.py
@@ -0,0 +1,17 @@
+import unittest
+from phrydy import MediaFile
+
+class TestPackageInterface(unittest.TestCase):
+    def setUp(self):
+        import os
+        mp3 = os.path.join(os.path.dirname(__file__), 'files','full.mp3')
+        self.media = MediaFile(mp3)
+
+    def test_title(self):
+        self.assertEqual(self.media.title, 'full')
+
+    def test_artitst(self):
+        self.assertEqual(self.media.artist, 'the artist')
+
+if __name__ == '__main__':
+    unittest.main()
