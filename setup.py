677cdaedbd3ecbc6c44f1057e46c7d11c6be9de9
Josef Friedrich <josef@friedrich.rocks>
2016-09-03 19:57:30 +0200

    Add option --long-description

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import os
 from setuptools import setup
 
-setup(name='phrydy',
-      version='0.0.1',
-      description='A easy wrapper for mutagen',
-      url='https://github.com/Josef-Friedrich/phrydy',
-      author='Josef Friedrich',
-      author_email='josef@friedrich.rocks',
-      license='MIT',
-      packages=['phrydy'],
-      install_requires=['mutagen','enum','six'],
-      zip_safe=False)
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name='phrydy',
+	version='0.0.1',
+	description='A easy wrapper for mutagen',
+	url='https://github.com/Josef-Friedrich/phrydy',
+	author='Josef Friedrich',
+	author_email='josef@friedrich.rocks',
+	license='MIT',
+	packages=['phrydy'],
+	long_description=read('README.rst'),
+	install_requires=['mutagen','enum','six'],
+	zip_safe=False
+)
