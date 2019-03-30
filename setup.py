b61a5e5a7c611d6d8aa9c056d7e36b214feac6fe
Josef Friedrich <josef@friedrich.rocks>
2019-03-30 18:34:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python
-
-import versioneer
 import os
-from setuptools import setup
 
+from setuptools import setup, find_packages
+import versioneer
 
 def read(file_name):
+    """
+    Read the contents of a text file and return its content.
+
+    :param str file_name: The name of the file to read.
+
+    :return: The content of the text file.
+    :rtype: str
+    """
     return open(
         os.path.join(os.path.dirname(__file__), file_name),
         encoding='utf-8'
@@ -21,7 +27,7 @@ setup(
     author='Josef Friedrich',
     author_email='josef@friedrich.rocks',
     license='MIT',
-    packages=['phrydy'],
+    packages=find_packages(),
     long_description=read('README.rst'),
     install_requires=[
         'mutagen>=1.39',
