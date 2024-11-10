bbbb66a4c0928206cbbd58308ef52be13bba707c
Josef Friedrich <josef@friedrich.rocks>
2024-11-10 13:17:29 +0100

    Test filething

--- tests/test_real_world.py
+++ tests/test_real_world.py
@@ -18,13 +18,13 @@ class TestBachWeihnachts:
         )
         assert "tests/files/real-world/Bach_Weihnachts.mp3" in mediafile.filename
 
-    # def test_constructor_filelike_object(self) -> None:
-    #     file = open(
-    #         Path(helper.TEST_RESOURCES_PATH) / "real-world" / "Bach_Weihnachts.mp3", "w"
-    #     )
-    #     mediafile = MediaFileExtended(file)
-
-    #     assert "tests/files/real-world/Bach_Weihnachts.mp3" in mediafile.filename
+    def test_constructor_filelike_object(self) -> None:
+        file = open(
+            Path(helper.TEST_RESOURCES_PATH) / "real-world" / "Bach_Weihnachts.mp3",
+            "rb",
+        )
+        mediafile = MediaFileExtended(file)
+        assert "tests/files/real-world/Bach_Weihnachts.mp3" in mediafile.filename
 
     def test_filename(self) -> None:
         assert "tests/files/real-world/Bach_Weihnachts.mp3" in self.mediafile.filename
