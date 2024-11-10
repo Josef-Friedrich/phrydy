afaed5651c27b31474d96a13971d6310eb445585
Josef Friedrich <josef@friedrich.rocks>
2024-11-10 13:06:17 +0100

    Add real world test files

---
+++ tests/test_real_world.py
@@ -0,0 +1,36 @@
+"""Test on real world meta data"""
+
+import os
+from pathlib import Path
+
+from phrydy import MediaFileExtended
+from tests import helper
+
+
+class TestBachWeihnachts:
+    mediafile = MediaFileExtended(
+        os.path.join(helper.TEST_RESOURCES_PATH, "real-world/Bach_Weihnachts.mp3")
+    )
+
+    def test_constructor_path(self) -> None:
+        mediafile = MediaFileExtended(
+            Path(helper.TEST_RESOURCES_PATH) / "real-world" / "Bach_Weihnachts.mp3"
+        )
+        assert "tests/files/real-world/Bach_Weihnachts.mp3" in mediafile.filename
+
+    # def test_constructor_filelike_object(self) -> None:
+    #     file = open(
+    #         Path(helper.TEST_RESOURCES_PATH) / "real-world" / "Bach_Weihnachts.mp3", "w"
+    #     )
+    #     mediafile = MediaFileExtended(file)
+
+    #     assert "tests/files/real-world/Bach_Weihnachts.mp3" in mediafile.filename
+
+    def test_filename(self) -> None:
+        assert "tests/files/real-world/Bach_Weihnachts.mp3" in self.mediafile.filename
+
+    def test_title(self) -> None:
+        assert (
+            self.mediafile.title
+            == 'Weihnachts-Oratorium, BWV 248: Teil I, I. Coro "Jauchzet, frohlocket!"'
+        )
